9e78bd6e2ecf645b4ae98f0b30d727d0061de82c
dev-kudli <sudarshan97.kudli@gmail.com>
2023-05-03 02:05:17 -0400

    Added airflow env variables

--- airflow/dags/staging_food_inspection.py
+++ airflow/dags/staging_food_inspection.py
@@ -3,11 +3,15 @@ from datetime import timedelta, datetime
 from sqlalchemy import create_engine
 
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 def stagingNycFoodInspections(user, password, host, port, db, table_name):
     try:
+        engine = create_engine(f'postgresql://{user}:{password}@{host}:{int(port)}/{db}')
+        print(f'Connected to {engine.url.database}')
+    
         url = 'https://data.cityofchicago.org/api/views/4ijn-s7e5/rows.tsv?accessType=DOWNLOAD&bom=true'
         data = pd.read_csv(url, sep='\t', low_memory=False)
         print('Row Count = ', len(data))
@@ -15,9 +19,6 @@ def stagingNycFoodInspections(user, password, host, port, db, table_name):
         data['Violations'] = data['Violations'].str.split('|')
         data_explode = data.explode('Violations')
         print('Row Count after explode = ', len(data_explode))
-
-        engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-        print(f'Connected to {engine.url.database}')
 
         data_explode.head(0).to_sql(name=table_name, con=engine)
         print('Table Created')
@@ -38,7 +39,7 @@ default_args = {
 }
 
 # Instantiate a DAG object
-staging_nyc_food_inspection_dag = DAG('staging_nyc_food_inspection',
+staging_food_inspection_dag = DAG('staging_food_inspection',
     default_args=default_args,
     description='Staging NYC Food Inspection',
     schedule_interval='@daily', 
@@ -46,24 +47,24 @@ staging_nyc_food_inspection_dag = DAG('staging_nyc_food_inspection',
     tags=['nyc, food, inspection']
 )
 
-with staging_nyc_food_inspection_dag:
+with staging_food_inspection_dag:
     # Creating first task
-    ingest_staging_nyc_food_inspection_task = PythonOperator(
-        task_id='staging_nyc_food_inspection', 
+    ingest_staging_food_inspection_task = PythonOperator(
+        task_id='staging_food_inspection', 
         python_callable=stagingNycFoodInspections, 
         op_kwargs=dict(
-            user = 'root',
-            password = 'root',
-            host = 'pgdatabase',
-            port = 5432,
-            db = 'nyc_food_inspection',
-            table_name = 'staging_chicago'
+            user = Variable.get('USER'),
+            password = Variable.get('PASSWORD'),
+            host = Variable.get('HOST'),
+            port = Variable.get('PORT'),
+            db = f"{Variable.get('DB')}_{Variable.get('ENV')}",
+            table_name = Variable.get('TABLE_NAME')
         ),
-        dag=staging_nyc_food_inspection_dag
+        dag=staging_food_inspection_dag
     )
 
     # Creating second task
-    end_task = DummyOperator(task_id='end_task', dag=staging_nyc_food_inspection_dag)
+    end_task = DummyOperator(task_id='end_task', dag=staging_food_inspection_dag)
 
     # Set the order of execution of tasks. 
-    ingest_staging_nyc_food_inspection_task >> end_task
+    ingest_staging_food_inspection_task >> end_task
